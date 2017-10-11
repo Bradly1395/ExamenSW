446f0f7f3cc4a3cc8ab69d1a2c0e4aa3be650585
Unknown <bradly_aguilera@yahoo.com>
2017-10-10 19:13:35 -0600

    Objeto Json

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,17 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+var Estudiante = {
+  nombre:"Bradly Josue Aguilera Inestroza",
+  cuenta:"1501199502119",
+  correo:"bradly_aguilera@yahoo.com"
+}
+
+router.get('/1501199502119',
+              function(req,res,next){
+                console.log(req.params);
+                res.json(Estudiante);
+              }
+);
+
 module.exports = router;
